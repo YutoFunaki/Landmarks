9910aad057c915b279a4ec067d2c1d43f7a7c169
YutoFunaki <kaizuka0629@gmail.com>
2023-02-04 17:19:50 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 船木勇斗 on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
