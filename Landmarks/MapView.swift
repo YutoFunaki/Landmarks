fd8def90eb8d61aee1e2097a5961da01463b531f
YutoFunaki <kaizuka0629@gmail.com>
2023-02-04 17:42:55 +0900

    section5終了

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 船木勇斗 on 2023/02/04.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    ) //マップの初期値を設定＆常に本体の位置を更新できる変数
+    
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
